c3b0d98671a21608885a3e243e312e115bf1402b
Mateus Machado <matteusmachhado@hotmail.com>
2024-11-10 16:06:01 -0300

    feat: product with gRPC service.

---
+++ src/services/Project.WMS.Product.WebAPI/Entities/Product.cs
@@ -0,0 +1,37 @@
+﻿using Projetct.WMS.Core.DomainObjects;
+
+namespace Project.WMS.Product.WebAPI.Entities
+{
+    public class Product : Entity
+    {
+        public Product(string name,
+            string description,
+            decimal price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+        private Product() { }
+
+        public string Name { get; private set; } = string.Empty;
+        public string Description { get; private set; } = string.Empty;
+        public decimal Price { get; private set; }
+        public DateTime? Disabled { get; private set; }
+
+        public void Update(string name,
+            string descrption,
+            decimal price)
+        {
+            Name = name;
+            Description = descrption;
+            Price = price;
+        }
+
+        public void Disable()
+        { 
+            Disabled = DateTime.UtcNow;
+        }
+    }
+}
---
+++ src/services/Project.WMS.Product.WebAPI/Services/gRPC/ProductGrpcService.cs
@@ -0,0 +1,38 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace Project.WMS.Product.WebAPI.Services.gRPC
+{
+    public class ProductGrpcService : Product.ProductBase
+    {
+        private readonly ILogger<ProductGrpcService> _logger;
+
+        public ProductGrpcService(ILogger<ProductGrpcService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ProductResponse> Get(GetProductRequest request, ServerCallContext context)
+        {
+            var random = new Random();
+
+            // todo: find database
+            var product = new Entities.Product($"Product", "Description", Math.Round((decimal)(random.NextDouble() * 100), 2));
+
+            var response = product ?? new Entities.Product("Product default", "Description default", 0.1m);
+
+            return Task.FromResult(MapProductToProductResponse(response));
+        }
+
+        private ProductResponse MapProductToProductResponse(Entities.Product product)
+        {
+            return new ProductResponse()
+            {
+                Name = product.Name,
+                Description = product.Description,
+                Price = (double)product.Price,
+                Disabled = product.Disabled.HasValue ? Timestamp.FromDateTime(product.Disabled.Value) : null,
+            };
+        }
+    }
+}
